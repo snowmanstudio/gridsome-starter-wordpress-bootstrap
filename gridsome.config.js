a3b762b952a16d0a21a8311414cedbb9d601a813
snowmanstudio <105069282+snowmanstudio@users.noreply.github.com>
2023-01-04 06:22:28 +0600

    oh no

--- gridsome.config.js
+++ gridsome.config.js
@@ -17,7 +17,7 @@ module.exports = {
     {
       use: '~/src/plugins/wp-source/',
       options: {
-        baseUrl: 'https://trickbd.com', // required - Replace me with your Wordpress URL 
+        baseUrl: 'https://picsart.cc', // required - Replace me with your Wordpress URL 
         typeName: 'WordPress', // GraphQL schema name (Optional)
         perPage: 10, // How many posts to load from server per request (Optional)
         concurrent: 10, // How many requests to run simultaneously (Optional)
